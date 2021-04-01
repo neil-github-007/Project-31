af374fe92e8948df3c161706bafe96ecbe23ac15
neil-github-007 <74409287+neil-github-007@users.noreply.github.com>
2021-04-01 18:29:11 +0530

    Update drops.js

--- drops.js
+++ drops.js
@@ -9,11 +9,13 @@ class Drops {
         World.add(world, this.body);
 
         this.drops = [];
+        
+        this.radius = 5;
     }
 
     display() {
         fill("blue");
-        ellipse(this.body.position.x, this.body.position.y, 10);
+        ellipse(this.body.position.x, this.body.position.y, this.radius, this.radius);
 
         if(this.body.position.y > 700) {
             Matter.Body.setPosition(this.body, {x: random(0, 400), y:0});
